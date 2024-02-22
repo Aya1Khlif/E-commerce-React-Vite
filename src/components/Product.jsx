75ad6e9cb9a88bad95edc3d1a362d2e5c5fa8936
Aya1Khlif <ayakhlife222@gmail.com>
2024-02-22 15:35:19 +0300

    add some plugin

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,32 +1,35 @@
-import {Link} from "react-router-dom";
-import '../../src/App.css'
+import { Link } from "react-router-dom";
+import "./product.css";
 import Card from "react-bootstrap/Card";
 export default function Product(props) {
-  
-  const {product,showButton} = props ;
+  const { product, showButton } = props;
   return (
     <>
-      <div className="container">
-   <div className="row">
-    <div className="col-lg-12  col-sm-12">
-    <Card className="card" >
-              <Card.Img variant="top" src={product.image}    />
-              <Card.Body >
+      <div className="">
+        <div className="row">
+          <div className="" style={{ height: "100%" }}>
+            <Card
+              className="card col-md-6 col-sm-12 "
+              style={{ width: "18rem" }}
+            >
+              <Card.Img variant="top" src={product.image} />
+              <Card.Body>
                 <Card.Title> {product.title}</Card.Title>
-                <Card.Text>
-                {product.description}
-                </Card.Text>
+                <Card.Text>{product.description}</Card.Text>
                 <p>Price : {product.price}</p>
-                {showButton &&  (
-                    <Link className="btn btn-primary" to={`/E-commerce-React-Vite/product/${product.id}`}>
+                {showButton && (
+                  <Link
+                    className="btn btn-primary"
+                    to={`/E-commerce-React-Vite/product/${product.id}`}
+                  >
                     Details
                   </Link>
-          )}
+                )}
               </Card.Body>
             </Card>
-    </div>
-   </div>
+          </div>
+        </div>
       </div>
     </>
-  )
+  );
 }
